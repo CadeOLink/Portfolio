3dfab376d651defc8db49df6d39c17617b01278c
Everton <83991145+CadeOLink@users.noreply.github.com>
2023-02-17 16:56:08 -0300

    Add files portfolio
    Adding files for to create repository portfolio

---
+++ app.js
@@ -0,0 +1,22 @@
+function social_medias(x){
+    if(x == 1){
+        window.location = "https://www.instagram.com/yosoyeverton/";
+    } if(x == 2){
+        window.location = "https://twitter.com/Coe_Everton";
+    } if(x == 3){
+        window.location = "https://github.com/CadeOLink";
+    } if(x == 4){
+        window.location = "https://cadeolink.github.io/Cracha_NLW/";
+    }
+}
+function projects(x){
+    if(x == 1){
+    window.location = "https://cadeolink.github.io/Cracha_NLW/";
+    } if(x == 2){
+    window.location = "https://cadeolink.github.io/Advice_generator/";
+    } if(x == 3){
+    window.location = "https://cadeolink.github.io/Order_Summary/";
+    } if(x == 4){
+    window.location = "https://cadeolink.github.io/Interaticve_rating-Frotend_mentor/";
+    }
+}
